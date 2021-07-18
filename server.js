9ad8137ca2fed25aacc620d3af3469c88909d255
karnowsky1 <kestime9015@bths.edu>
2021-07-18 19:40:30 -0400

    made more port changes

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 
+const PORT = process.env.PORT || 3001;
+
 const app = express();
 
 const { animals } = require('./data/animals');
-
-const PORT = process.env.PORT || 3001;
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
